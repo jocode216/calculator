0fadce634cd778dd9ee3db23d1364ae2f5a9eb04
JoCode <tekajoseph5@gmail.com>
2025-08-28 08:57:55 +0300

    Calculator for roadmap demo
    roadmap demo calcoaltor

---
+++ script.js
@@ -0,0 +1,21 @@
+
+        function appendToDisplay(value) {
+            const display = document.getElementById('display');
+            display.value += value;
+        }
+
+        function clearDisplay() {
+            const display = document.getElementById('display');
+            display.value = '';
+        }
+
+        function calculateResult() {
+            const display = document.getElementById('display');
+            try {
+                display.value = eval(display.value);
+            } catch (error) {
+                display.value = 'Null';
+            }
+        }
+        
+    
